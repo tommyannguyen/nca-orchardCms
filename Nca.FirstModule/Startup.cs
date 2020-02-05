e7603f0ee85371a2837f62ac112dc5b526115055
An Nguyen Cao <anguyencao@win.ips-ag.net>
2020-02-05 16:10:18 +0700

    first commit

---
+++ Nca.FirstModule/Startup.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nca.FirstModule
+{
+    public class Startup
+    {
+    }
+}
---
+++ Templates/Nca.MainTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Nca Main Theme",
+    Author = "The Nca Team",
+    Website = "",
+    Version = "0.0.1",
+    Description = "Main Theme"
+)]
